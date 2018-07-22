53f7039532b18a0e5e118eca7abbedff3977915b
eolculnamo2 <rbertram8@gmail.com>
2018-07-21 23:38:44 -0500

    fix meta

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import React from 'react'
 import { BrowserRouter as Router, Link, Route, withRouter, Redirect } from 'react-router-dom'
+import DocumentMeta from 'react-document-meta';
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import './scss/main.scss'
@@ -12,8 +13,18 @@ import Admin from './components/Admin/Admin';
 
 class App extends React.Component {
     render() {
+        const meta = {
+            title: "Sword Point",
+            description: "Read, share, discuss, and publish about Historical European Martials Arts (HEMA) also known as the Martial Arts of Renaissance Europe. Getting started with HEMA?  Check out our beginner's guides? Experienced renaissance martial artist? Why not submit an article?",
+            meta: {
+                charset: 'utf-8',
+                name: {
+                keywords: "HEMA, ARMA, longsword, spear, sword, buckler, pike, staff, mma, martial arts, wrestling, grappling, knight, knights, boxing, fighting, fight, learn, gear, fencing"
+                }
+            }
+        }    
         return(
-            <div>
+            <DocumentMeta {...meta}>
                 <Header />
                     <Route exact path='/' component={LandingPage} />
                     <Route exact path='/articles' component={Articles} />
@@ -26,7 +37,7 @@ class App extends React.Component {
                         <Admin />
                     )} />
                 <Footer />
-            </div>
+            </DocumentMeta>
         )
     }
 }
