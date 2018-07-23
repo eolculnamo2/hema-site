ee4299d8f51173e264a67a5bad590edacbd08123
eolculnamo2 <rbertram8@gmail.com>
2018-07-22 19:54:10 -0500

    fi

--- src/components/Contribute/Contribute.js
+++ src/components/Contribute/Contribute.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
 import Row1 from './subcomponents/Row1'
 import Row2 from './subcomponents/Row2'
 import Row3 from './subcomponents/Row3'
@@ -8,6 +9,9 @@ class Contribute extends React.Component {
     render() {  
         return(
             <div>
+                <Helmet>
+                    <script src="https://www.google.com/recaptcha/api.js" async defer></script> 
+                </Helmet>
                 <Row1 />
                 <Row2 />
                 <Row3 />
