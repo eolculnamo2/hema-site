f945e129c9198583b155ee018ca761e8ec005d00
eolculnamo2 <rbertram8@gmail.com>
2018-08-30 01:24:35 -0500

    split chunks

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,12 @@ module.exports = {
     entry: './src/Index.js',
     output: {
         path: path.resolve('./assets/dist'),
-        filename: 'bundle.js'
+        filename: '[name].[chunkhash].bundle.js'
+    },
+    optimization: {
+        splitChunks: {
+          chunks: 'all'
+        }
     },
     module: {
         rules: [
@@ -27,28 +32,5 @@ module.exports = {
                 loader: 'style-loader!css-loader!sass-loader'
             }
         ]
-    },
-    optimization: {
-        splitChunks: {
-          chunks: 'async',
-          minSize: 30000,
-          maxSize: 0,
-          minChunks: 1,
-          maxAsyncRequests: 5,
-          maxInitialRequests: 3,
-          automaticNameDelimiter: '~',
-          name: true,
-          cacheGroups: {
-            vendors: {
-              test: /[\\/]node_modules[\\/]/,
-              priority: -10
-            },
-            default: {
-              minChunks: 2,
-              priority: -20,
-              reuseExistingChunk: true
-            }
-          }
-        }
-      }
+    }
 }
