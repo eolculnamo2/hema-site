5ff399f9ec88d74bb86d491c3c0986cc1ed5cd85
eolculnamo2 <rbertram8@gmail.com>
2019-03-30 00:09:03 -0500

    eslint progress

--- src/components/Header/js/mobileMenuConfig.js
+++ src/components/Header/js/mobileMenuConfig.js
@@ -1,55 +1,53 @@
 export default [
-    {
-      name: "Home",
-      link: '/',
-      subcategories: []
-    },
+  {
+    name: 'Home',
+    link: '/',
+    subcategories: [],
+  },
 
-    {
-      name: "Articles",
-      subcategories: [
-        {
-          subName: "View All",
-          link: "/articles"
-        },
-        {
-          subName: "Publish",
-          link: "/contribute"
-        }
-      ]
-    },
+  {
+    name: 'Articles',
+    subcategories: [
+      {
+        subName: 'View All',
+        link: '/articles',
+      },
+      {
+        subName: 'Publish',
+        link: '/contribute',
+      },
+    ],
+  },
 
-    {
-      name: "Tournaments",
-      subcategories: [
-        {
-          subName: "About",
-          link: "/about-tournaments"
-        },
-        {
-          subName: "Registration",
-          link: "/event-registration"
-        },
-        {
-          subName: "Manage Events",
-          link: "/create-tournament"
-        }
-      ]
-    },
+  {
+    name: 'Tournaments',
+    subcategories: [
+      {
+        subName: 'About',
+        link: '/about-tournaments',
+      },
+      {
+        subName: 'Registration',
+        link: '/event-registration',
+      },
+      {
+        subName: 'Manage Events',
+        link: '/create-tournament',
+      },
+    ],
+  },
 
-    {
-      name: "Account",
-      subcategories: [
-        {
-          subName: "Login",
-          link: "/login"
-        },
-        {
-          subName: "Register",
-          link: "/register"
-        }
-      ]
-    }
-
-  ];
-  
+  {
+    name: 'Account',
+    subcategories: [
+      {
+        subName: 'Login',
+        link: '/login',
+      },
+      {
+        subName: 'Register',
+        link: '/register',
+      },
+    ],
+  },
+];
